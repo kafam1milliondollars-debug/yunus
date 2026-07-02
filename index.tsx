ab32ff9868041878739c88e9802b471671beb08f
kafam1milliondollars-debug <kafam1milliondollars@gmail.com>
2026-07-02 16:38:16 +0300

    Update index.tsx
    şlöiŞ:Ç

--- index.tsx
+++ index.tsx
@@ -1,19 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-// React 18 Root Mounting
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error("Failed to find the root element");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
